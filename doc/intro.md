0c7e40398821a291cfbbecb000a0005d933b308d
bernie <bernie@tutanota.com>
2018-03-12 09:42:43 +0100

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simpleblog
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/simpleblog/core.clj
@@ -0,0 +1,7 @@
+(ns simpleblog.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Let's create a blog page!"))
