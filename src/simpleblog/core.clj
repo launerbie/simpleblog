12e6f9fc425ea7b13f6e1f0f0ec750d9c2ac569d
bernie <bernie@tutanota.com>
2018-03-16 20:02:03 +0100

    Add route blog entry

--- src/simpleblog/core.clj
+++ src/simpleblog/core.clj
@@ -8,7 +8,8 @@
   (GET "/" [] {:status 200
                :headers {"Content-Type" "text/plain"}
                :body    "<h1> My simple blog page </h1>"})
-  (GET "/posts" [] "<h1> My simple blog page </h1>")
+  (GET "/posts" [] "Show index of blog posts here.")
+  (GET "/posts/:id" [id] (str "Blog entry number: " (str id)))
   (GET "/test/:name" [name] (str "Hello " name))
   (GET "/test" [] "test"))
 
